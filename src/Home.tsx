f1240178169fd93220695392b491c8b7ce0010f5
keith-loww <keithlow1@hotmail.com>
2023-10-20 11:08:27 -0700

    header

---
+++ src/Home.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
+        <div className="flex flex-wrap p-8">
+            <h1 className="flex w-full justify-center text-4xl font-bold">
+                CodeLab Toolkit Sandboxes 🧰
+            </h1>
+            <Link className="w-full" to="/usecontext/usage">
+                useContext Usage
+            </Link>
+            <Link className="w-full" to="/usecontext/change">
+                useContext Change
+            </Link>
+            <Link className="w-full" to="/usecontext/parent">
+                useContext Parent
+            </Link>
+            <Link className="w-full" to="/usecontext/bad">
+                useContext Bad
+            </Link>
+            <Link className="w-full" to="/usecontext/posts">
+                useContext Posts
+            </Link>
+            <Link className="w-full" to="/tailwind/color">
+                Tailwind Color
+            </Link>
+            <Link className="w-full" to="/tailwind/font">
+                Tailwind Font
+            </Link>
+            <Link className="w-full" to="/custom-hooks/counter">
+                Custom Hooks Counter
+            </Link>
+            <Link className="w-full" to="/custom-hooks/theme">
+                Custom Hooks Theme
+            </Link>
+            <Link className="w-full" to="/usereducer/counter">
+                useReducer Counter
+            </Link>
+            <Link className="w-full" to="/usereducer/context">
+                useReducer Context
+            </Link>
+            <Link className="w-full" to="/usereducer/posts">
+                useReducer Posts
+            </Link>
+        </div>
+    );
+}
